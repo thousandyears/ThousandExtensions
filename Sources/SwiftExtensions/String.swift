f63c9ac6e1a0ab6be37129dbcfb4c3fe75352127
Ste Prescott <ste@thousandyears.co.uk>
2020-05-19 23:54:25 +0100

    Add extension to titlecase strings

--- Sources/SwiftExtensions/String.swift
+++ Sources/SwiftExtensions/String.swift
@@ -3,4 +3,12 @@ extension String {
     public static func * (l: String, r: Int) -> String {
         repeatElement(l, count: r).joined()
     }
+    
+    public func titlecase() -> String {
+      return prefix(1).uppercased() + lowercased().dropFirst()
+    }
+
+    public mutating func titlecase() {
+      self = titlecase()
+    }
 }
