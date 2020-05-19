eadd272c960580da72392ebe1cc3d904fcb07c6c
Ste Prescott <ste@thousandyears.co.uk>
2020-05-19 23:54:09 +0100

    Add URL extension to subscript query parameters

--- Sources/FoundationExtensions/URL.swift
+++ Sources/FoundationExtensions/URL.swift
@@ -6,4 +6,11 @@ extension URL {
         o.appendPathExtension(ext)
         return o
     }
+    
+    public subscript(queryParam: String) -> String? {
+        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: false)
+            else { return nil }
+        
+        return components.queryItems?.first(where: { $0.name == queryParam })?.value
+    }
 }
