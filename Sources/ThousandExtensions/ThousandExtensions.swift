1654f99937f9e74a1d271799d6aeb2aa2e7df627
milos <milos@screensailor.net>
2020-05-19 19:20:57 +0100

    declare operators

--- Sources/ThousandExtensions/ThousandExtensions.swift
+++ Sources/ThousandExtensions/ThousandExtensions.swift
@@ -2,3 +2,21 @@
 @_exported import SKExtensions
 @_exported import Periscope
 @_exported import TrySwitch
+
+// TrySwitch
+infix operator ?= : AssignmentPrecedence
+
+// DictionaryArithmetic
+infix operator .+ : AdditionPrecedence
+infix operator .- : AdditionPrecedence
+infix operator .* : MultiplicationPrecedence
+infix operator ./ : MultiplicationPrecedence
+
+infix operator .+= : AssignmentPrecedence
+infix operator .-= : AssignmentPrecedence
+infix operator .*= : AssignmentPrecedence
+infix operator ./= : AssignmentPrecedence
+
+// Space
+infix operator ± : RangeFormationPrecedence
+infix operator ±= : AssignmentPrecedence
