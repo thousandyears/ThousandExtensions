859209dbace80472785402293b6b15943eabbadc
Oliver Atkinson <oliveratkinson@me.com>
2020-05-20 23:54:12 +0100

    Add sugar for assigning multiple keypaths in one expression

--- Sources/CombineExtensions/Publisher+Assign.swift
+++ Sources/CombineExtensions/Publisher+Assign.swift
@@ -9,14 +9,44 @@
 
 import Combine
 
-public extension Publisher where Self.Failure == Never {
+extension Publisher where Self.Failure == Never {
     
-    func assign(_ assignments: Assign<Output>...) -> AnyCancellable {
+    public func assign(_ assignments: Assign<Output>...) -> AnyCancellable {
         sink { value in
             for o in assignments {
                 o.action(value)
             }
         }
+    }
+    
+    @inlinable public func assign<Root>(
+        to keyPath: ReferenceWritableKeyPath<Root, Output>, on object: Root, ownership: Assign<Output>.Ownership = .weak
+    ) -> AnyCancellable where Root: AnyObject {
+        assign(
+            .to(keyPath, on: object, ownership: ownership)
+        )
+    }
+    
+    @inlinable public func assign<Root1, Root2>(
+        to  keyPath1: ReferenceWritableKeyPath<Root1, Output>, on object1: Root1, ownership ownership1: Assign<Output>.Ownership = .weak,
+        and keyPath2: ReferenceWritableKeyPath<Root2, Output>, on object2: Root2, ownership ownership2: Assign<Output>.Ownership = .weak
+    ) -> AnyCancellable where Root1: AnyObject, Root2: AnyObject {
+        assign(
+            .to(keyPath1, on: object1, ownership: ownership1),
+            .to(keyPath2, on: object2, ownership: ownership2)
+        )
+    }
+    
+    @inlinable public func assign<Root1, Root2, Root3>(
+        to  keyPath1: ReferenceWritableKeyPath<Root1, Output>, on object1: Root1, ownership ownership1: Assign<Output>.Ownership = .weak,
+        and keyPath2: ReferenceWritableKeyPath<Root2, Output>, on object2: Root2, ownership ownership2: Assign<Output>.Ownership = .weak,
+        and keyPath3: ReferenceWritableKeyPath<Root3, Output>, on object3: Root3, ownership ownership3: Assign<Output>.Ownership = .weak
+    ) -> AnyCancellable where Root1: AnyObject, Root2: AnyObject, Root3: AnyObject {
+        assign(
+            .to(keyPath1, on: object1, ownership: ownership1),
+            .to(keyPath2, on: object2, ownership: ownership2),
+            .to(keyPath3, on: object3, ownership: ownership3)
+        )
     }
 
 }
@@ -30,31 +60,39 @@ public struct Assign<Output> {
     }
     
     public var action: (Output) -> Void
-    public init<Root>(keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) {
-        action = { root[keyPath: keyPath] = $0 }
+    
+    public init<Root>(keyPath: ReferenceWritableKeyPath<Root, Output>, on object: Root) {
+        action = { object[keyPath: keyPath] = $0 }
     }
     
-    public init<Root>(keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root, ownership: Ownership = .strong) where Root: AnyObject {
+    public init<Root>(
+        keyPath: ReferenceWritableKeyPath<Root, Output>,
+        on object: Root,
+        ownership: Ownership = .strong
+    ) where Root: AnyObject {
         switch ownership {
         case .strong:
-            action = { [root] in root[keyPath: keyPath] = $0 }
+            action = { [object] in object[keyPath: keyPath] = $0 }
         case .weak:
-            action = { [weak root] in root?[keyPath: keyPath] = $0 }
+            action = { [weak object] in object?[keyPath: keyPath] = $0 }
         case .unowned:
-            action = { [unowned root] in root[keyPath: keyPath] = $0 }
+            action = { [unowned object] in object[keyPath: keyPath] = $0 }
         }
     }
     
-    public static func to<Root>(_ keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) -> Assign<Output> {
-        .init(keyPath: keyPath, on: root)
+    @inlinable public static func to<Root>(
+        _ keyPath: ReferenceWritableKeyPath<Root, Output>,
+        on object: Root
+    ) -> Assign<Output> {
+        .init(keyPath: keyPath, on: object)
     }
     
-    public static func to<Root>(
+    @inlinable public static func to<Root>(
         _ keyPath: ReferenceWritableKeyPath<Root, Output>,
-        on root: Root,
+        on object: Root,
         ownership: Ownership = .strong
     ) -> Assign<Output> where Root: AnyObject {
-        .init(keyPath: keyPath, on: root)
+        .init(keyPath: keyPath, on: object)
     }
 
 }
